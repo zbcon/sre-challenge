3ede9a2ad0baac4fc32f0937ce54e632513aa3c0
Alex <alxj.harrison@gmail.com>
2022-06-04 13:55:23 +0100

    feat: Add cmds to deploy Deployments & Services
    Script deployments & services for invoice-app & payment-provider.
    
    Use 'apply' over 'create' to allow easier configuration changes during development

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-# this is your part to fill
+## Deploy invoice-app & Service
+kubectl apply -f invoice-app/deployment.yaml
+kubectl apply -f invoice-app/service.yaml
+
+## Deploy payment-provider app & Service
+kubectl apply -f payment-provider/deployment.yaml
+kubectl apply -f payment-provider/service.yaml
